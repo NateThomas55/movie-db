efc917b969d2c2a5bb72bd77f6840dbb87be4624
Nathaniel Thomas <thomasnathaniel10@gmail.com>
2020-02-29 13:07:38 -0500

    init server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import { importSchema } from 'graphql-import'
+import { makeExecutableSchema } from 'graphql-tools'
 import 'dotenv/config'
 
-console.log('hello')
+import resolvers from '../src/resolvers'
+
+async function start() {
+  // instance of express server
+  const app = express()
+  //importSchema imports schema
+  const typeDefs = await importSchema('./src/types/schema.graphql')
+  const Resolvers = resolvers
+  // stitch the schemas together
+  const schema = makeExecutableSchema({ typeDefs, Resolvers })
+  // apollo server
+  const server = new ApolloServer({ schema })
+
+  server.applyMiddleware({ app })
+
+  //start server
+  app.listen({ port: process.env.URL }, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:${process.env.URL}${server.graphqlPath}`
+    )
+  })
+}
+
+start().catch(err => console.log(err))
